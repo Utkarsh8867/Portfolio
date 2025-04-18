bf673b0c8b4516113f1b117b99d4ec717f9f18fc
Utkarsh Kadu <145374112+Utkarsh8867@users.noreply.github.com>
2025-04-18 12:07:47 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a Engineerng student <br className='sm:block hidden' />
-            pursuing B.Tech in Information Technology
+            pursuing B.Tech in Computer Engineering
           </p>
         </div>
       </div>
